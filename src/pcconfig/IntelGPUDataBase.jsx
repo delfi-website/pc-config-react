90c7e93a9027bf37e30af44efd6ed2b54767551e
Delfi-CH <laspitzer@gmx.ch>
2025-04-11 15:07:26 +0200

    Added Intel GPU logic

--- src/pcconfig/IntelGPUDataBase.jsx
+++ src/pcconfig/IntelGPUDataBase.jsx
@@ -4,28 +4,67 @@ export default function IntelGPUDataBase({gpuGeneration, performanceBraket}) {
     const [gpuOptions, setGpuOptins] = useState({
         gpuOptionA: null,
         gpuOptionB: null,
-        gpuOptionC: null
+        gpuOptionC: null,
+        gpuOptionD: null
     })
     useEffect(()=>{
         //GPU LIST
 
         //Battlemage
-        const gpu01 = "Arc 5 B580"
-        const gpu02 = "Arc 5 B570"
+        const gpu01 = "| Arc 5 B580 "
+        const gpu02 = "| Arc 5 B570 "
 
         //Alchemist
-        const gpu03 = "Arc 7 A770 16GB"
-        const gpu04 = "Arc 7 A770 08GB"
-        const gpu05 = "Arc 7 A750"
-        const gpu06 = "Arc 5 A580"
-        const gpu07 = "Arc 3 A380"
-        const gpu08 = "Arc 3 A310"
-    })
+        const gpu03 = "| Arc 7 A770 16GB "
+        const gpu04 = "| Arc 7 A770 08GB "
+        const gpu05 = "| Arc 7 A750 "
+        const gpu06 = "| Arc 5 A580 "
+        const gpu07 = "| Arc 3 A380 "
+        const gpu08 = "| Arc 3 A310 "
+
+        if (gpuGeneration === 0 && performanceBraket !== 3) {
+            setGpuOptins({
+                gpuOptionA: "| none ",
+                gpuOptionB: null,
+                gpuOptionC: null,
+                gpuOptionD: null,
+            })
+        } else if(gpuGeneration === 0 && performanceBraket === 3) {
+            setGpuOptins({
+                gpuOptionA: gpu01,
+                gpuOptionB: gpu02,
+                gpuOptionC: null,
+                gpuOptionD: null
+            })
+        } else if(gpuGeneration === 1 && performanceBraket !== 3 && performanceBraket !== 5) {
+        setGpuOptins({
+            gpuOptionA: "| none",
+            gpuOptionB: null,
+            gpuOptionC: null,
+            gpuOptionD: null
+        })
+    } else if(gpuGeneration === 1 && performanceBraket == 3) {
+            setGpuOptins({
+                gpuOptionA: gpu03,
+                gpuOptionB: gpu04,
+                gpuOptionC: gpu05,
+                gpuOptionD: gpu06
+            })
+    } else if(gpuGeneration === 1 && performanceBraket == 5) {
+            setGpuOptins({
+                gpuOptionA: gpu07,
+                gpuOptionB: gpu08,
+                gpuOptionC: null,
+                gpuOptionD: null
+            })
+        }
+        }, [gpuGeneration, performanceBraket])
 
     return(
         <div>
             <p>Fitting GPU's are:</p>
-            <p>{gpuOptions.gpuOptionA} {gpuOptions.gpuOptionB} {gpuOptions.gpuOptionC}</p>
+            <p>{gpuOptions.gpuOptionA} {gpuOptions.gpuOptionB} {gpuOptions.gpuOptionC}|</p>
+            <p>Note: Intel currently only offers GPU's in the 60 and 30 Performance Class.</p>
         </div>
     )
 }
